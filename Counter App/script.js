56dfd96faae688fc55d9a095321ccbea3d6217e7
Abhimannu Singh Kunwar <155934864+Abhimannu09dev@users.noreply.github.com>
2025-02-16 23:31:18 +0545

    Update script.js

--- Counter App/script.js
+++ Counter App/script.js
@@ -1,18 +1,26 @@
+// Select the buttons and the count display element
 let increase = document.querySelector("#increase");
 let decrease = document.querySelector("#decrease");
 let reset=document.querySelector("#reset");
-let i = 0;
+let i = 0;// Initialize the counter variable to 0
+
+// Event listener for the "Increase" button
 increase.addEventListener("click", (event) => {
-    i++
+    i++ // Increment the counter by 1
+    // Update the display with the new count
     document.querySelector("#count").innerHTML = `Count: ${i}`;
 })
 
+// Event listener for the "Decrease" button
 decrease.addEventListener("click", (event) =>{
-    i--
+    i-- // Decrement the counter by 1
+    // Update the display with the new count
     document.querySelector("#count").innerHTML = `Count ${i}`;
 })
 
+// Event listener for the "Reset" button
 reset.addEventListener("click", ()=>{
-    i=0
+    i=0 // Reset the counter to 0
+    // Update the display with the new count
     document.querySelector("#count").innerHTML = `Count ${i}`;
 })
