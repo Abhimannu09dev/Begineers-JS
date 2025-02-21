7f7dd4e69efc2d4bdf43f18614ac93b4cc261c92
Abhimannu Singh Kunwar <155934864+Abhimannu09dev@users.noreply.github.com>
2025-02-21 20:41:40 +0545

    Added Comments in script.js file

--- Scissor Paper Rock/script.js
+++ Scissor Paper Rock/script.js
@@ -1,25 +1,32 @@
+// Selecting all the buttons and display elements
 const buttons = document.querySelectorAll(".moves");
 const score = document.querySelector("#score");
 let scoreBoard = document.querySelector("#scoreBoard");
 
+// Initializing all the necessary components
 let userchoice = "";
 let choice = "";
 let userScore = 0;
 let computerScore = 0;
 let draw = 0;
 
+// Add event listers for all the buttons
 buttons.forEach(choice => {
     choice.addEventListener("click", () => {
         userchoice = choice.querySelector("img").alt;
-        playGame(userchoice);
+        playGame(userchoice); // Calling the main function play games
     })
 })
 
+// Creation of function play games
 const playGame = (userchoice) => {
+    // Initializing the choices for computer
     const computerOptions = ["rock", "paper", "scissors"]
+    // Choosing a choice of computer
     let computerChoice = Math.floor(Math.random() * 3);
     choice = (computerOptions[computerChoice]);
 
+    // Check either the user won loss or draw
     if (userchoice === choice) {
         let count = 1;
         draw += count;
@@ -36,11 +43,12 @@ const playGame = (userchoice) => {
         let count = 1;
         computerScore += count;
     }
-    Result(userchoice, choice);
-    updateScore();
+    Result(userchoice, choice); // Function call for result
+    updateScore(); // Function call for updateScore
     return;
 }
 
+// Creation of function for Result
 const Result = (userchoice, choice) => {
     score.innerHTML = `
         Your Choice:  <img src="./images/${userchoice}.png" alt="${userchoice}">
@@ -49,10 +57,12 @@ const Result = (userchoice, choice) => {
     return;
 }
 
+// Creation of function to update Score
 const updateScore = () => {
     scoreBoard.innerHTML = `WIn: ${userScore} | Draw: ${draw} | Loss: ${computerScore}`;
 }
 
+// Creation of reset event Listener
 const reset = document.querySelector("#Reset")
 reset.addEventListener("click", () => {
     userScore = 0;
